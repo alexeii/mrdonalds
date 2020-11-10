66787a4a89ce97953bd581ebfbdc1577d80cb76e
Alexey Arhipiv <alex3171@list.ru>
2020-11-10 17:23:54 +0500

    feat:useFetch+preloader

--- src/Components/Hooks/Menu/Menu.js
+++ src/Components/Hooks/Menu/Menu.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Banner} from './BannerMenu'
-import DBMenu from '../../DBMenu';
 import {ListItem} from './ListItem';
+import {useFetch} from '../useFetch';
+import Loader from 'react-loader-spinner'
 
 
 const MenuStyled = styled.main`
@@ -15,25 +16,41 @@ const SectionMenu = styled.section`
     padding: 30px;
 `;
 
-export const Menu = ({setOpenItem}) => (
+export const Menu = ({setOpenItem}) => {
+    const res = useFetch();
+    const dbMenu = res.response;
+    
+    return(
     <MenuStyled>
         <Banner/>
-    <SectionMenu>
+        {res.response ?
+        <>
+        <SectionMenu>
         <h2>Бургеры</h2>
         <ListItem 
-            itemList={DBMenu.burger}
+            itemList={dbMenu.burger}
             setOpenItem={setOpenItem}    
         />
-    </SectionMenu>
-    <SectionMenu>
-        <h2>Закуски / Напитки</h2>
-        <ListItem 
-            itemList={DBMenu.other}
-            setOpenItem={setOpenItem}
-        />
-    </SectionMenu>
+        </SectionMenu>
+        <SectionMenu>
+            <h2>Закуски / Напитки</h2>
+            <ListItem 
+                itemList={dbMenu.other}
+                setOpenItem={setOpenItem}
+            />
+        </SectionMenu>
+        </> : res.error ? 
+        <div>Sorry,we will fix it soon...</div>:
+        
+        <Loader type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} />
+    }
         
         
     </MenuStyled>
 
-);
+)
+};
